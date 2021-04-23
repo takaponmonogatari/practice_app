dcfcd6e65362b88e82e30c323c2f5b79adec466e
takaponmonogatari <taka11hiro26@gmail.com>
2021-04-23 08:26:35 +0000

    [Start] 1

---
+++ ruby_practice/helloruby.rb
@@ -0,0 +1,12 @@
+print("Hello, Ruby.\n")
+print("Hello,\nRuby\n!\n")
+print("Hello \\ Ruby!\n")
+print("Hello, \\ \"Ruby\".\n" )
+print "Hello, Ruby.\n"
+print "Hello,\nRuby\n!\n"
+puts "Hello,Ruby!"
+puts "Hello, ", "Ruby!"
+puts "100"
+puts 100
+p "100"
+p 100
